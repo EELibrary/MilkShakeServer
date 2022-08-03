557710e0c39dc0ffb379da292df95965c76090d9
wangxyper <8943419+wangxyperQwQ@user.noreply.gitee.com>
2022-08-03 19:02:00 +0800

    Revert "Fix some concurrently problems"
    This reverts commit c4f567ddff65d331428941ad98e5dea3a5e0f753.

--- src/main/java/catserver/server/launch/Java11Support.java
+++ src/main/java/catserver/server/launch/Java11Support.java
@@ -11,7 +11,6 @@ import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Java11Support {
