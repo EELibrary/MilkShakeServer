60f905af325793328bfc1e8f19da0fc5db2c36cc
wangxyper <8943419+wangxyperQwQ@user.noreply.gitee.com>
2022-08-03 19:09:29 +0800

    Revert "Add some javadocs"
    This reverts commit 4ac3d7dffa45e17166d93f3f9d085237aa8a546f.

--- src/main/java/gg/eilsapgroup/milkshake/errorrecoder/ErrorRecoder.java
+++ src/main/java/gg/eilsapgroup/milkshake/errorrecoder/ErrorRecoder.java
@@ -7,9 +7,6 @@ import java.util.Date;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
-/**
- * 错误记录器
- */
 public class ErrorRecoder implements Runnable {
     private final Queue<String> errorMessages = new ConcurrentLinkedDeque<>();
     private FileOutputStream currentOutStream;
@@ -22,16 +19,14 @@ public class ErrorRecoder implements Runnable {
                 this.close();
                 break;
             }
-            //当错误消息队列的大小大于100时才会写入文件,不过感觉这样并不太好
             if(errorMessages.size()>100){
                 for (;;){
                     String errorMessage = errorMessages.poll();
                     if (errorMessage==null)break;
-                    this.writeErrorMessage(errorMessage);
+
                 }
                 continue;
             }
-            //空闲的时候休眠线程
             try{
                 Thread.sleep(0,1);
             }catch (Exception e){
@@ -40,10 +35,6 @@ public class ErrorRecoder implements Runnable {
         }
     }
 
-    /**
-     * 初始化文件
-     * @param recordFileName
-     */
     private void init(String recordFileName){
         File file = new File(new File("mklogs"),recordFileName);
         try{
@@ -59,10 +50,6 @@ public class ErrorRecoder implements Runnable {
         }
     }
 
-    /**
-     * 向文件写入错误信息
-     * @param message
-     */
     private void writeErrorMessage(String message){
         if (message!=null){
             String newMessageWithNewLine = message + "\n";
@@ -75,9 +62,6 @@ public class ErrorRecoder implements Runnable {
         }
     }
 
-    /**
-     * 关闭记录器
-     */
     private void close(){
         try {
             if (!this.errorMessages.isEmpty()) {
