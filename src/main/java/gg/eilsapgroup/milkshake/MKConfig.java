34c2a596d771f42fd89096d46bb38555afba687f
wangxyper <8943419+wangxyperQwQ@user.noreply.gitee.com>
2022-08-03 19:09:18 +0800

    Revert "Revert "Some thing fixed and async something""
    This reverts commit 60b29a699a4f5f2cf93068a542991e5da44798ee.

--- src/main/java/gg/eilsapgroup/milkshake/MKConfig.java
+++ src/main/java/gg/eilsapgroup/milkshake/MKConfig.java
@@ -11,6 +11,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * 配置文件和错误日志记录器都在这个类里
@@ -26,6 +27,11 @@ public class MKConfig {
     public static final MKThreadGroup workerGroup = new MKThreadGroup();
     public static ErrorRecoder errorLogsRecoder = new ErrorRecoder();
 
+
+    public static ForkJoinPool entitiesExecutor = new ForkJoinPool(Runtime.getRuntime().availableProcessors(),serverWorkerFactory,null,false);
+    public static ForkJoinPool trackerPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors(),serverWorkerFactory,null,false);
+    public static ForkJoinPool serverWorldExecutor = new ForkJoinPool(Runtime.getRuntime().availableProcessors(),serverWorkerFactory,null,false);
+
     /**
      * 加载所有的配置或者初始化配置
      */
@@ -37,6 +43,7 @@ public class MKConfig {
                 executorConfig = config.createSection("executors");
                 executorConfig.set("entities-worker-threads",Runtime.getRuntime().availableProcessors()+2);
                 executorConfig.set("tracker-worker-threads",Runtime.getRuntime().availableProcessors());
+                executorConfig.set("env-worker-threads",Runtime.getRuntime().availableProcessors());
                 executorConfig.set("disable-async-catcher",false);
                 debugConfig = config.createSection("debug");
                 debugConfig.set("enable-error-logs",false);
@@ -45,6 +52,9 @@ public class MKConfig {
             }
             config.load(configFile);
             executorConfig = config.getConfigurationSection("executors");
+            entitiesExecutor = new ForkJoinPool(executorConfig.getInt("entities-worker-threads"),serverWorkerFactory,null,false);
+            trackerPool = new ForkJoinPool(executorConfig.getInt("tracker-worker-threads"),serverWorkerFactory,null,false);
+            serverWorldExecutor = new ForkJoinPool(executorConfig.getInt("env-worker-threads"),serverWorkerFactory,null,false);
             disableAsyncCatcher = executorConfig.getBoolean("disable-async-catcher");
             debugConfig = config.getConfigurationSection("debug");
             if (debugConfig.getBoolean("enable-error-logs")){
